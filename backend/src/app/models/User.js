c521f1bfe56eb80683c92aa953d58d0a55e7b52f
Lucas Maciel <luk4xm4ci3l@gmail.com>
2024-03-03 13:17:17 -0300

    feat(user): implemented user creation

---
+++ backend/src/app/models/User.js
@@ -0,0 +1,26 @@
+import Sequelize, { Model } from 'sequelize';
+import bcrypt from 'bcrypt';
+
+export class User extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        name: Sequelize.STRING,
+        email: Sequelize.STRING,
+        password: Sequelize.VIRTUAL,
+        password_hash: Sequelize.STRING,
+        admin: Sequelize.BOOLEAN
+      },
+      { sequelize }
+    );
+
+    this.addHook('beforeSave', async user => {
+      user.password_hash = await bcrypt.hash(user.password, 10);
+    });
+
+    return this;
+  }
+  checkPassword(password) {
+    return bcrypt.compare(password, this.password_hash);
+  }
+}

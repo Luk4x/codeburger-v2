e1372af1898865953da7154d5ad8a3cfe0486a36
Lucas Maciel <luk4xm4ci3l@gmail.com>
2024-03-03 13:01:51 -0300

    chore(config): general config

---
+++ backend/src/config/database.js
@@ -0,0 +1,9 @@
+module.exports = {
+  dialect: 'postgres',
+  url: 'postgresql://postgres:1C4ga3deBFbFD6feAbGeEGCdEe5gea61@viaduct.proxy.rlwy.net:27081/railway',
+  define: {
+    timespamps: true,
+    underscored: true,
+    underscoredAll: true
+  }
+};

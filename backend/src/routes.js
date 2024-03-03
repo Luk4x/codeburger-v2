7f3df7746a61ccd86ce3058d0c7d07feea218949
Lucas Maciel <luk4xm4ci3l@gmail.com>
2024-03-03 13:10:59 -0300

    feat(routes): defined routes

---
+++ backend/src/routes.js
@@ -0,0 +1,37 @@
+import { Router } from 'express';
+
+import multer from 'multer';
+import multerConfig from './config/multer';
+
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+import ProductController from './app/controllers/ProductController';
+import CategoryController from './app/controllers/CategoryController';
+import OrderController from './app/controllers/OrderController';
+
+import authMiddleware from './app/middlewares/auth';
+
+const upload = multer(multerConfig);
+
+const routes = new Router();
+
+routes.post('/users', UserController.store);
+
+routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
+
+routes.post('/products', upload.single('file'), ProductController.store);
+routes.put('/products/:id', upload.single('file'), ProductController.update);
+routes.delete('/products/:id', ProductController.delete);
+routes.get('/products', ProductController.index);
+
+routes.post('/categories', upload.single('file'), CategoryController.store);
+routes.put('/categories/:id', upload.single('file'), CategoryController.update);
+routes.get('/categories', CategoryController.index);
+
+routes.post('/orders', OrderController.store);
+routes.put('/orders/:id', OrderController.update);
+routes.get('/orders', OrderController.index);
+
+export default routes;
